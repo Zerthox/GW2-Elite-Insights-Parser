dac4af80dd895da373f4c8e178258d58738f36d8
EliphasNUIT <bichon.jekfer@gmail.com>
2026-08-23 01:12:47 +0200

    improved mirage and mesmer shatter detection (less false negatives)

--- GW2EIEvtcParser/EIData/ProfHelpers/Mesmer/MesmerHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/Mesmer/MesmerHelper.cs
@@ -49,14 +49,14 @@ internal static class MesmerHelper
         // Shatters
         new EffectCastFinder(MindWrack, EffectGUIDs.MesmerDistortionOrMindWrack)
             .UsingSrcSpecsChecker([Spec.Mirage, Spec.Mesmer])
-            .UsingNoSecondaryEffectSameSrcChecker(EffectGUIDs.MesmerThePrestigeDisappear2AndNonChronoShatterAroundClones)
+            .UsingNoSecondaryEffectSameSrcCheckerOnSamePosition(EffectGUIDs.MesmerThePrestigeDisappear2AndNonChronoShatterAroundClones)
             .UsingChecker((evt, combatData, agentData, skillData) => !combatData.HasGainedBuff(DistortionBuff, evt.Src, evt.Time)),
         new EffectCastFinder(CryOfFrustration, EffectGUIDs.MesmerCryOfFrustration)
             .UsingSrcSpecsChecker([Spec.Mirage, Spec.Mesmer])
-            .UsingNoSecondaryEffectSameSrcChecker(EffectGUIDs.MesmerThePrestigeDisappear2AndNonChronoShatterAroundClones),
+            .UsingNoSecondaryEffectSameSrcCheckerOnSamePosition(EffectGUIDs.MesmerThePrestigeDisappear2AndNonChronoShatterAroundClones),
         new EffectCastFinder(Diversion, EffectGUIDs.MesmerDiversion)
             .UsingSrcSpecsChecker([Spec.Mirage, Spec.Mesmer])
-            .UsingNoSecondaryEffectSameSrcChecker(EffectGUIDs.MesmerThePrestigeDisappear2AndNonChronoShatterAroundClones),
+            .UsingNoSecondaryEffectSameSrcCheckerOnSamePosition(EffectGUIDs.MesmerThePrestigeDisappear2AndNonChronoShatterAroundClones),
         new EffectCastFinder(DistortionSkill, EffectGUIDs.MesmerDistortionOrMindWrack)
             .UsingSrcSpecsChecker([Spec.Mirage, Spec.Mesmer])
             .UsingChecker((evt, combatData, agentData, skillData) => combatData.HasGainedBuff(DistortionBuff, evt.Src, evt.Time))
